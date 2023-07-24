d9698405f4fb6c95d8e47a6621978b0702237a53
Foitelija <foitelija777@gmail.com>
2023-07-24 09:52:31 +0300

    update: required purchase fields

--- HomeAccounting.Domain/Purchases/PurchaseCreate.cs
+++ HomeAccounting.Domain/Purchases/PurchaseCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,11 @@ namespace HomeAccounting.Domain.Purchases
 {
     public class PurchaseCreate
     {
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Должно быть более 0")]
         public int CategoryId { get; set; }
+        [Required]
+        [Range(0.01, 999999, ErrorMessage = "Должно быть более 0")]
         public decimal Price { get; set; }
         public string? Comment { get; set; }
     }
